28293f0fb2c7c20e2a3fea946fb563758c83ba9d
Veronika <0.zeromech.0@gmail.com>
2021-03-13 18:00:10 +0200

    changes

--- TT.py
+++ TT.py
@@ -7,10 +7,11 @@ world.set_world_background_color_rgb(180, 45, 255)
 num = sprite.add_sprite("X_X_pushka", 80, 600)
 sprite.set_bottom_to(num, 600)
 sprite.set_left_to(num, 0)
+ball=sprite.add_sprite("spring",100,400)
 
-
-@wrap_py.on_key_down(wrap_py.K_RIGHT,wrap_py.K_LEFT)
-def dvijenie_pushki():
-    sprite.move_sprite_by(num,10,0)
-    sprite.move_sprite_by(num,-10,0)
-
+@wrap_py.on_key_down(wrap_py.K_RIGHT, wrap_py.K_LEFT)
+def dvijenie_pushki(key):
+    if key == wrap_py.K_RIGHT:
+        sprite.move_sprite_by(num, 10, 0)
+    if key == wrap_py.K_LEFT:
+        sprite.move_sprite_by(num, -10, 0)
