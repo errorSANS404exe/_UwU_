838a74c1c1b1c5882b8e7a701dcd81fb1a290ee5
Veronika <0.zeromech.0@gmail.com>
2021-05-30 12:57:40 +0300

    changes

--- TT.py
+++ TT.py
@@ -8,16 +8,17 @@ pushka_UwU = sprite.add_sprite("X_X_pushka", 80, 600)
 sprite.set_bottom_to(pushka_UwU, 600)
 sprite.set_left_to(pushka_UwU, 0)
 ball = sprite.add_sprite("spring", 300, 0)
+ballhp = sprite.add_text(300, 0, "3",text_color=[255,255,255],font_size=25)
 scorost = 0
 bullet = None
 bullets = []
 balls = []
-d = {"number": ball, "scorost": 0, "scorostx": 5}
+d = {"number": ball, "scorost": 0, "scorostx": 5, "ballhp": ballhp}
 balls.append(d)
 
-
 ball2 = sprite.add_sprite("spring", 200, 400)
-b = {"number": ball2, "scorost": 0, "scorostx": -8}
+ballhp2=sprite.add_text(200,400,"5",text_color=[255,255,255],font_size=25)
+b = {"number": ball2, "scorost": 0, "scorostx": -8,"ballhp":ballhp2}
 balls.append(b)
 
 
@@ -27,7 +28,7 @@ def beskonechnoe_dvijenie_karika():
     for ball in balls:
         sprite.move_sprite_by(ball["number"], ball["scorostx"], ball["scorost"])
         ball["scorost"] += 1
-       # print(ball["scorost"])
+        # print(ball["scorost"])
         # отбивка от низа
         niz = sprite.get_bottom(ball["number"])
         if niz >= 600:
@@ -48,7 +49,7 @@ def beskonechnoe_dvijenie_karika():
         if left <= 0:
             sprite.set_left_to(ball["number"], 0)
             ball["scorostx"] = -ball["scorostx"]
-
+        sprite.move_sprite_to(ball["ballhp"],sprite.get_sprite_x(ball["number"]),sprite.get_sprite_y(ball["number"]))
 
 @wrap_py.on_mouse_move
 def mouse_move(pos):
